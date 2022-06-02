c76439cae145c62ed4a5f7b627c55e27dc0f0e23
danielnascimentotomaz <danielsabio1000@hotmail.com>
2022-06-02 10:38:34 -0300

    PROJETO EM ANDAMENTO

---
+++ AULAS E EXERCICIOS/01_PRIMEIRO_PROGRAMA/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+// PACOTE
+package primeiroprograma;
+// CLASSE
+public class PrimeiroPrograma {
+    // MÉTODO PRINCIPAL
+    public static void main(String[] args) {
+        System.out.println("OLÁ! MUNDO");
+       
+    }
+    
+}
---
+++ AULAS E EXERCICIOS/02-TÉCNICA-CAMELCASE/src/camelcase/CamelCase.java
@@ -0,0 +1,39 @@
+/*
+                TÉCNICA DO CAMELCASE
+ 
+ =>=> PRIMEIRA LETRA MAIÚSCULA:
+ 
+ºClasse 0U interface => AlunoCursoEmVideo
+  
+=>=> PRIMEIRA LETRA MINÚSCULA E AS OUTRAS PALAVRA USANDO A TÉCNICA CAMELCASE
+*    =======================================================================
+*º Atributo => nomeAluno
+* 
+*º Variável => médiaPrimeiroBimestre
+* 
+*ºMétodo => lancarNotaAluno()
+* 
+* =>=> TODAS LETRAS MINÚSCULA
+*      ======================
+* ºpacote => alunocursoemvideo
+* 
+* =>=> TODAS LETRAS MAIÚSCULA
+* ===========================
+* º CONSTANTE => VALOR_DE_PI
+       
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
