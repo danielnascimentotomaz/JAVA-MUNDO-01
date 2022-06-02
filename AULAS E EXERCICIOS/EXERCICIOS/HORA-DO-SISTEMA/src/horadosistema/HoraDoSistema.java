aff7f44fa17b4debb67116f57b42c6d4d3e94871
danielnascimentotomaz <danielsabio1000@hotmail.com>
2022-06-02 11:52:58 -0300

    PROJETO ANDAMENTO

---
+++ AULAS E EXERCICIOS/EXERCICIOS/HORA-DO-SISTEMA/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,28 @@
+
+package horadosistema;
+
+import java.util.Date;
+
+
+public class HoraDoSistema {
+    
+
+    public static void main(String[] args) {
+        String resultado;
+        Date relogio = new Date();
+        resultado = relogio.toString();
+        
+        
+        System.out.println("A HORA DO SISTEM É:");
+        System.out.println(resultado);
+
+        
+        
+        
+        
+        
+        
+
+    }
+    
+}
