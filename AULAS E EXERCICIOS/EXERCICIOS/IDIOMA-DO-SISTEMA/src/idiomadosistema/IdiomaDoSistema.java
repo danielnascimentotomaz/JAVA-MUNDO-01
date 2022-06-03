3b43074424334cd79c34d46792ee0555a963df21
danielnascimentotomaz <danielsabio1000@hotmail.com>
2022-06-03 07:56:14 -0300

    PROJETO EM ANDAMENTO

---
+++ AULAS E EXERCICIOS/EXERCICIOS/IDIOMA-DO-SISTEMA/src/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,19 @@
+
+package idiomadosistema;
+
+import java.util.Locale;
+
+
+public class IdiomaDoSistema {
+
+   
+    public static void main(String[] args) {
+        Locale idioma =  Locale.getDefault();
+        System.out.println("SEU SISTEMA ESTÁ EM: "+ idioma.toString()+" "+ idioma.getDisplayLanguage());
+  
+        
+        
+        
+    }
+    
+}
